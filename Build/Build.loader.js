67ef91e4e36315dc680167add4a2c62539ce1cc4
Danil Kulagin <d.kulagin@emehanika.com>
2026-05-04 14:41:12 +0700

    Add Unity WebGL build

--- Build/Build.loader.js
+++ Build/Build.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 77158577,
+    wasmFileSize: 77163869,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
